3f3674fab7e5b0da5c9fa2ad766e71cf79e4e569
Mirobecatherline <catemirobe@gmail.com>
2024-09-23 23:44:22 +0300

    edit config

--- _config.js
+++ _config.js
@@ -2,8 +2,9 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://cate:ene1048cate@cluster0.o8x3d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    development: 'mongodb+srv://cate:ene1048cate@cluster0.o8x3d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    test: 'mongodb+srv://cate:ene1048cate@cluster0.o8x3d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+
 }
 module.exports = config;
